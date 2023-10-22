f2c4d611395c096f5dd4186a07cb1517df8fcbb7
danielPaszek <daniel.paszek@student.put.poznan.pl>
2023-10-22 21:55:58 +0200

    fix redirect after register

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/app');
 });
+Route::get('/login', function () {
+    return redirect(route('filament.admin.auth.login'));
+})->name('login');
